cb04584153ee3380fb0195649bf1ef2405f8cd84
Nimarho <andyschima@gmail.com>
2023-01-23 15:37:47 +0100

    mise en place du hamburger menu et des routes

--- client/src/components/header/Navbar.tsx
+++ client/src/components/header/Navbar.tsx
@@ -1,21 +1,20 @@
 import React from 'react'
-import { Outlet, Link } from "react-router-dom";
-
+import { Link } from "react-router-dom";
+import { useState } from "react";
 
 export default function Navbar() {
-
+  const [isNavOpen, setIsNavOpen] = useState(false);
   return (
    <>
 <nav className="fixed top-0 left-0 z-20 w-full border-b border-gray-200 bg-white py-2.5 px-6 sm:px-4">
   <div className="container mx-auto flex max-w-6xl flex-wrap items-center justify-between">
-    <a href="#" className="flex items-center">
+    <Link to={`/`} className="flex items-center">
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="mr-3 h-6 text-blue-500 sm:h-9">
         <path stroke-linecap="round" stroke-linejoin="round" d="M21 7.5l-9-5.25L3 7.5m18 0l-9 5.25m9-5.25v9l-9 5.25M3 7.5l9 5.25M3 7.5v9l9 5.25m0-9v9" />
       </svg>
-      <Link to={`/`}>
-         <span className="self-center whitespace-nowrap text-xl font-semibold">PokeMarket</span>
-      </Link>
-    </a>
+        <span className="self-center whitespace-nowrap text-xl font-semibold">PokeMarket</span>
+    </Link>
+    
     <div className="mt-2 sm:mt-0 sm:flex md:order-2">
       <Link to={`/Login`}>
         <button type="button" className="rounde mr-3 hidden border border-blue-700 py-1.5 px-6 text-center text-sm font-medium text-blue-700 focus:outline-none focus:ring-4 focus:ring-blue-300 md:inline-block rounded-lg">Login</button>
@@ -23,11 +22,12 @@ export default function Navbar() {
       <Link to={`/Register`}>
         <button type="button" className="rounde mr-3 hidden bg-blue-700 py-1.5 px-6 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 md:mr-0 md:inline-block rounded-lg">Register</button>
       </Link>
-      <button data-collapse-toggle="navbar-sticky" type="button" className="inline-flex items-center rounded-lg p-2 text-sm text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 md:hidden" aria-controls="navbar-sticky" aria-expanded="false">
+      <button data-collapse-toggle="navbar-sticky" type="button" className="inline-flex items-center rounded-lg p-2 text-sm text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 md:hidden" aria-controls="navbar-sticky" aria-expanded="false" onClick={() => setIsNavOpen((prev) => !prev)}>
         <span className="sr-only">Open main menu</span>
         <svg className="h-6 w-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
       </button>
-      <div className="hidden w-full items-center justify-between md:order-1 md:flex md:w-auto" id="navbar-sticky">
+    </div>
+    <div className="hidden w-full items-center justify-between md:order-1 md:flex md:w-auto" id="navbar-sticky">
       <ul className="mt-4 flex flex-col rounded-lg border border-gray-100 bg-gray-50 p-4 md:mt-0 md:flex-row md:space-x-8 md:border-0 md:bg-white md:text-sm md:font-medium">
         <li>
           <Link to={`/Buy`} className="block rounded py-2 pl-3 pr-4 text-gray-700 hover:bg-gray-100 md:p-0 md:hover:bg-transparent md:hover:text-blue-700" aria-current="page">Buy</Link>
@@ -38,9 +38,62 @@ export default function Navbar() {
        
       </ul>
     </div>
-    </div>
+    <div className={isNavOpen ? "showMenuNav" : "hideMenuNav"}>
+            <div
+              className="absolute top-0 right-0 px-8 py-8"
+              onClick={() => setIsNavOpen(false)}
+            >
+              <svg
+                className="h-8 w-8 text-gray-600"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <line x1="18" y1="6" x2="6" y2="18" />
+                <line x1="6" y1="6" x2="18" y2="18" />
+              </svg>
+            </div>
+            <ul className="flex flex-col items-center justify-between min-h-[250px]">
+              <li className="border-b border-gray-400 my-8 uppercase">
+                <Link to={`/Buy`} onClick={() => setIsNavOpen(false)} className="block rounded py-2 pl-3 pr-4 text-gray-700 hover:bg-gray-100 md:p-0 md:hover:bg-transparent md:hover:text-blue-700" aria-current="page">Buy</Link>
+              </li>
+              <li className="border-b border-gray-400 my-8 uppercase">
+                <Link to={`/Sell`} onClick={() => setIsNavOpen(false)} className="block rounded py-2 pl-3 pr-4 text-gray-700 hover:bg-gray-100 md:p-0 md:hover:bg-transparent md:hover:text-blue-700" aria-current="page">Sell</Link>
+              </li>
+              <li className="border-b border-gray-400 my-8 uppercase">
+                <Link to={`/Login`} onClick={() => setIsNavOpen(false)} className="block rounded py-2 pl-3 pr-4 text-gray-700 hover:bg-gray-100 md:p-0 md:hover:bg-transparent md:hover:text-blue-700" aria-current="page">Login</Link>
+              </li>
+              <li className="border-b border-gray-400 my-8 uppercase">
+                <Link to={`/Register`} onClick={() => setIsNavOpen(false)} className="block rounded py-2 pl-3 pr-4 text-gray-700 hover:bg-gray-100 md:p-0 md:hover:bg-transparent md:hover:text-blue-700" aria-current="page">Register</Link>
+              </li>
+            </ul>
+          </div>
   </div>
 </nav>
+<style>{`
+      .hideMenuNav {
+        display: none;
+      }
+      .showMenuNav {
+        display: block;
+        position: absolute;
+        width: 100%;
+        height: 100vh;
+        top: 0;
+        left: 0;
+        background: white;
+        z-index: 10;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-evenly;
+        align-items: center;
+      }
+    `}</style>
+
+
 
    </>
   )
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,9 +2,12 @@
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
+    "./node_modules/flowbite/**/*.{js,jsx,ts,tsx}"
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin')
+],
 }

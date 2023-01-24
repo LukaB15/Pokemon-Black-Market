8279fcc1945465c2ccb9fbaa667c8b4bc23f3154
Nimarho <andyschima@gmail.com>
2023-01-24 11:29:11 +0100

    debut singlepkmn

--- client/src/routes/SinglePokemon.tsx
+++ client/src/routes/SinglePokemon.tsx
@@ -1,6 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import { Params, useParams } from 'react-router-dom';
+
 
 export default function SinglePokemon() {
+    const [pokemon, setPokemon] = useState(null);
+    const id:string | undefined = useParams().id;
+    useEffect(()=>{
+    const fetchPokemon = async (id:string | undefined) => {
+        const response = await axios ({
+            method:"get",
+            url:`https://pokeapi.co/api/v2/pokemon/${id}`
+        })
+        return(response.data);
+    }
+    fetchPokemon(id);
+    console.log(pokemon);
+    
+    })
   return (
     <div>SinglePokemon</div>
   )

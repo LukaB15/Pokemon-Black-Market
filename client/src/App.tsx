696f1198c998bbeafc03ab170291425734da2cec
LukaB15 <lukabrabant15@gmail.com>
2023-01-25 09:16:25 +0100

    merge

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from 'react';
 import Navbar from './components/header/Navbar';
 import { Outlet } from "react-router-dom";
 import Footer from './components/footer/Footer';
+// import Footer from './components/footer/Footer';
 
 function App() {
   const [data, setData] = useState(null);

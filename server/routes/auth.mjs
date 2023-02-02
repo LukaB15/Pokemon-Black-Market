f4444b5c2e123349b600ecb86779664ade335e82
Colin <colin25.huart@hotmail.com>
2023-02-02 16:15:36 +0100

    repared

--- server/routes/auth.mjs
+++ server/routes/auth.mjs
@@ -10,7 +10,7 @@ router.post("/register", async (req, res) => {
   let passwFormat = /^[A-Za-z]\w{7,30}$/;
   const password = req.body.password;
   console.log(req.body.password);
-  
+
   if (!password.match(passwFormat)) return "Invalid password";
   console.log("apres le if");
   const hashing = await bcrypt.hash(password, 10);
@@ -25,7 +25,6 @@ router.post("/register", async (req, res) => {
   } catch (err) {
     res.status(500).json(err);
   }
-  
 });
 
 //login
@@ -51,7 +50,6 @@ router.post("/login", async (req, res) => {
       { expiresIn: "1d" }
     );
     const { hidepassword, ...others } = user;
-    res.cookie("jwt", accesToken, { httpOnly: true, maxAge: 3000 });
     res.status(200).json({ ...others, accesToken });
   } catch (err) {
     res.status(500).json(err);

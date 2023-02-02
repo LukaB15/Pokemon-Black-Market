29fd301a77c2fec48c6a6d2fb1306baa7208b317
Colin <colin25.huart@hotmail.com>
2023-02-02 16:24:49 +0100

    correct

--- server/routes/auth.mjs
+++ server/routes/auth.mjs
@@ -3,7 +3,6 @@ import express from "express";
 const router = express.Router();
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
-import cookieParser from "cookieParser";
 
 //register
 router.post("/register", async (req, res) => {
@@ -50,6 +49,7 @@ router.post("/login", async (req, res) => {
       { expiresIn: "1d" }
     );
     const { hidepassword, ...others } = user;
+    res.cookie("jwt", accesToken, { httpOnly: true, maxAge: 3000 });
     res.status(200).json({ ...others, accesToken });
   } catch (err) {
     res.status(500).json(err);

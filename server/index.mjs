bf413996a3ee3c1a2f2cbec9c88089462881bed8
Colin <colin25.huart@hotmail.com>
2023-02-08 14:24:39 +0100

    deploy try

--- server/index.mjs
+++ server/index.mjs
@@ -30,6 +30,6 @@ app.use("/api/user", userRoute);
 app.use("/api/auth", authRoute);
 app.use("/api/order", orderRoute);
 
-app.listen(process.env.PORT || 3001, () => {
+app.listen(process.env.PORT, () => {
   console.log("Backend server is running!");
 });

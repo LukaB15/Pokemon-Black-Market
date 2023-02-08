fdb1188fae80663198809f9b53f4733cbacea4f9
Colin <colin25.huart@hotmail.com>
2023-02-08 14:45:37 +0100

    deploy

--- client/src/features/buyList/buyListSliceAPI.tsx
+++ client/src/features/buyList/buyListSliceAPI.tsx
@@ -2,7 +2,8 @@ import axios from "axios";
 
 export async function getBuyListAsync() {
   const response = await axios.get(
-    `https://black-market-pokemon-server.onrender.com/api/product/`
+    `https://black-market-pokemon-server.onrender.com/api/product/`,
+    { withCredentials: true }
   );
   return response.data;
 }
